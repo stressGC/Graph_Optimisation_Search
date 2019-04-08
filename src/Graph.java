f30f6037a138977a99d95b8595b50c50510f5c9f
stressGC <g.cosson@live.fr>
2019-04-08 15:10:11 -0400

    doc & comments

--- src/Graph.java
+++ src/Graph.java
@@ -6,12 +6,25 @@ import java.util.ArrayList;
  */
 
 public class Graph {
+	
+	/* attributes */
     private ArrayList<Node> nodes;
+    public static int maxDistance = Integer.MAX_VALUE;
     
+    /**
+     * Graph constructor
+     * @param nodes ArrayList<Node> of all the nodes contained in the graph
+     */
     public Graph(ArrayList<Node> nodes) {
     	this.nodes = nodes;
     }
     
+    /**
+     * adds an edge between two Nodes in the graph
+     * @param nodeNameA name of the Node A
+     * @param nodeNameB name of the Node B
+     * @param distance distance between both Nodes
+     */
     public void addEdgeBetween(String nodeNameA, String nodeNameB, int distance) {
     	Node nodeA = this.getNodeByName(nodeNameA);
     	Node nodeB = this.getNodeByName(nodeNameB);
@@ -21,6 +34,13 @@ public class Graph {
     	nodeB.addEdgeTo(nodeA, distance);
     } 
     
+    /**
+     * Searches recursively in the graph to find the path between two Nodes
+     * @param from Node from where the message is emmitted
+     * @param dest Node where the message is heading
+     * @param pr PathResult object containing the current infos on the search
+     * @return PathResult containing the result of the search
+     */
     private PathResult recursiveSearch(Node from, Node dest, PathResult pr) {
     	pr.addNode(from);
     	// we got dest directly from here
@@ -34,6 +54,11 @@ public class Graph {
     	return recursiveSearch(nextNode, dest, pr);
     }
     
+    /**
+     * wrapper function for cleaner recursiveSearch prints etc
+     * @param nodeNameA name of the node A
+     * @param nodeNameB name of the node B
+     */
     public void sendMessageBetween(String nodeNameA, String nodeNameB) {
     	Node src = this.getNodeByName(nodeNameA);
     	Node dest = this.getNodeByName(nodeNameB);
@@ -45,12 +70,20 @@ public class Graph {
     	
     }
     
+    /**
+     * prints all Nodes in graph, debug purpose
+     */
     public void printAllNodes() {
     	for(Node n : this.nodes) {
     		System.out.println(n.toString());
     	}
     }
     
+    /**
+     * utility function to retrieve a Node by its name
+     * @param name name of the Node we are looking for
+     * @return Node corresponding to name
+     */
     private Node getNodeByName(String name) {
     	for(Node n : this.nodes) {
     		if(name.equals(n.getName())) {
--- src/Node.java
+++ src/Node.java
@@ -6,34 +6,47 @@ import java.util.ArrayList;
  */
 
 public class Node {
+	
+	/* attributes */
 	private String name;
 	private ArrayList<Edge> edges;
 	private int score = 0;
 	
+	/* getters / setters */
 	public int getScore() {
 		return score;
 	}
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
-	public void addScore() {
-		this.setScore(this.getScore() + 1);
-	}
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * Node constructor
+	 * @param name name of the Node
+	 */
 	public Node(String name) {
 		this.name = name;
 		this.edges = new ArrayList<Edge>();
 	}
 	
+	/**
+	 * 
+	 * @param to
+	 * @param distance
+	 */
 	public void addEdgeTo(Node to, int distance) {
 		Edge newEdge = new Edge(to, distance);
 		this.edges.add(newEdge);
 	}
-	 
+	
+	/**
+	 * Checks if the current Node has the destination in its direct links
+	 * @param dest destination Node
+	 * @return boolean if true or false
+	 */
 	public boolean hasDestinationAsNeighbor(Node dest) {
 		for(Edge e : edges) {
 			if(dest.getName().equals(e.getLinkedNode().getName())) {
@@ -43,12 +56,15 @@ public class Node {
 		return false;
 	}
 	
+	/**
+	 * @return Node return closest Node to the current One
+	 */
 	public Node getNextNode() {
 		Node closestNode = null;
 		int closestDistance = Integer.MAX_VALUE;
 		
 		for(Edge e : this.edges) {
-			if (e.getDistance() < closestDistance) {
+			if (e.getDistance() < closestDistance && e.getDistance() <= Graph.maxDistance) {
 				closestNode = e.getLinkedNode();
 				closestDistance = e.getDistance();
 			}
@@ -56,6 +72,16 @@ public class Node {
 		return closestNode;
 	}
 	
+	/**
+	 * adds score to the current Node
+	 */
+	public void addScore() {
+		this.setScore(this.getScore() + 1);
+	}
+	
+	/**
+	 * @return String the Node as a string for debugging purpose
+	 */
 	public String toString() {
 		String message = "\nNode : '" + this.name + "'";
 		
