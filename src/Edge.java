7dd0865d77c79f8e47306500ff897859cc94aa3a
stressGC <g.cosson@live.fr>
2019-04-08 15:23:37 -0400

    doc & comments

--- src/Edge.java
+++ src/Edge.java
@@ -4,22 +4,33 @@
  */
 
 public class Edge {
+	
+	/* attributes */
 	private int distance;
 	private Node to;
 	
-	public int getDistance() {
-		return this.distance;
-	}
-	
-	public Node getLinkedNode() {
-		return this.to;
-	}
-	
+	/**
+	 * Edge constructor
+	 * @param to Node
+	 * @param distance
+	 */
 	public Edge(Node to, int distance) {
 		this.to = to;
 		this.distance = distance;
 	}
 	
+	/* setters / getters */
+	public int getDistance() {
+		return this.distance;
+	}
+	public Node getLinkedNode() {
+		return this.to;
+	}
+	
+	/**
+	 * returns a String representing the Edge object
+	 * @returns String 
+	 */
 	public String toString() {
 		return "-> " + to.getName() + " [" + distance + "]";
 	}
