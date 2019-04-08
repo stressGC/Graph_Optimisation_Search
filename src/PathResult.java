a98adf6b05639384d12860401d37a5bc7e39eac6
stressGC <g.cosson@live.fr>
2019-04-08 15:12:40 -0400

    doc & comments

--- src/PathResult.java
+++ src/PathResult.java
@@ -6,25 +6,42 @@ import java.util.ArrayList;
  */
 
 public class PathResult {
+	
+	/* attributes */
 	private ArrayList<Node> nodes;
 	
+	/**
+	 * PathResult constructor w/o args
+	 */
 	public PathResult() {
 		this.nodes = new ArrayList<Node>();
 	}
 	
+	/**
+	 * PathResult constructor with args
+	 * @param nodes Nodes forming the path
+	 */
 	public PathResult(ArrayList<Node> nodes) {
 		this.nodes = nodes;
 	}
 	
+	/* setters / getters */
 	public void setNodes(ArrayList<Node> nodes) {
 		this.nodes = nodes;
 	}
 	
+	/**
+	 * adds a new Node to the resulting path
+	 * @param node Node to add to the path
+	 */
 	public void addNode(Node node) {
 		node.addScore();
 		this.nodes.add(node);
 	}
-		
+	
+	/**
+	 * returns the PathResult object as a string
+	 */
 	public String toString() {
 		if(this.nodes.size() == 0) {
 			return "Aucun chemin trouvé";
